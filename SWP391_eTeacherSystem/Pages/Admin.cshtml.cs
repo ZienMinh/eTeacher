13f05de5434c72187185a3c327e2509d0b7121fb
ZienMinh <phongdtse180404@fpt.edu.vn>
2024-06-25 21:45:22 +0700

    feat: Admindashboard

--- SWP391_eTeacherSystem/Pages/Admin.cshtml.cs
+++ SWP391_eTeacherSystem/Pages/Admin.cshtml.cs
@@ -1,12 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services;
 
 namespace SWP391_eTeacherSystem.Pages
 {
+
     public class AdminModel : PageModel
     {
+        private readonly VisitorCounterService _visitorCounterService = new VisitorCounterService();
+
+        public int TotalVisitors { get; set; }
         public void OnGet()
         {
+            TotalVisitors = _visitorCounterService.TotalVisitors;
         }
     }
 }
--- SWP391_eTeacherSystem/Pages/Index.cshtml.cs
+++ SWP391_eTeacherSystem/Pages/Index.cshtml.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services;
 
 namespace SWP391_eTeacherSystem.Pages
 {
     public class IndexModel : PageModel
     {
-        
+        private readonly VisitorCounterService _visitorCounterService = new VisitorCounterService();
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -15,7 +16,7 @@ namespace SWP391_eTeacherSystem.Pages
 
         public void OnGet()
         {
-
+            _visitorCounterService.IncrementVisitorCount();
         }
 
       
